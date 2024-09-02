3af9e4fd2ebd1a34de81042265bc7f77f0d11ee3
aman <=amansinghstm012@gmail.com>
2024-09-02 22:48:04 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://welcomed-pig-75.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
